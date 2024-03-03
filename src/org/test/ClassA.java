8ef12e92d1b43af90d697c3bbdf0c3219b0cacab
SinthuPriya <sinthuja.engg@gmail.com>
2024-03-03 13:28:58 +0530

    Customised message

---
+++ src/org/test/ClassA.java
@@ -0,0 +1,10 @@
+package org.test;
+
+public class ClassA {
+
+	public static void main(String[] args) {
+		String s = "priya";
+	    System.out.println(s);
+	}
+
+}
---
+++ src/org/test/ClassB.java
@@ -0,0 +1,11 @@
+package org.test;
+
+public class ClassB {
+
+	public static void main(String[] args) {
+    String s = "sinthuja";
+    System.out.println(s);
+    
+	}
+
+}
