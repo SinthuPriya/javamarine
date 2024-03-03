02762aff098917d46e72f780480dd07e3fe8305a
SinthuPriya <sinthuja.engg@gmail.com>
2024-03-03 14:20:59 +0530

    marine2

--- src/org/test/ClassA.java
+++ src/org/test/ClassA.java
@@ -5,6 +5,7 @@ public class ClassA {
 	public static void main(String[] args) {
 		String s = "priya";
 	    System.out.println(s);
+	    System.out.println("sinthujapriyavikki");
 	}
 
 }
--- src/org/test/ClassB.java
+++ src/org/test/ClassB.java
@@ -3,7 +3,7 @@ package org.test;
 public class ClassB {
 
 	public static void main(String[] args) {
-    String s = "sinthuja";
+    String s = "sinthuja conflict marine2 message";
     System.out.println(s);
     
 	}
